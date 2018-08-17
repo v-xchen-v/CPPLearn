1968e501e9e4cd06d0dbdb069fa8d372e863d6a3
Your Name <you@example.com>
2018-08-17 18:06:08 +0800

    test

--- thread/multithread/thread_create_close/main.cpp
+++ thread/multithread/thread_create_close/main.cpp
@@ -23,17 +23,3 @@ int main()
     cout << "Hello World!" << endl;
     return 0;
 }
-
-//谷歌拼音输入法
-
-//1,通过指令：
-
-//sudo apt-get install fcitx-googlepinyin
-
-//2， 在settings -> language support 里将 keyboard input method system 设置为fcitx
-
-//3， 重启
-
-//在settings-> Text Entry 里，添加输入源， 搜索 google-pinyin，添加。
-
-//4， 按Ctrl +Space 可以中英文输入切换
